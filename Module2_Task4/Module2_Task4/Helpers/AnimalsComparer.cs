6b49ae575ba2e5e59360fb13fa96a2badb405497
Vira Vostrikova <vvostrikova@csod.com>
2021-02-26 13:29:22 +0200

    Added sorting and filtering methods

---
+++ Module2_Task4/Module2_Task4/Helpers/AnimalsComparer.cs
@@ -0,0 +1,27 @@
+﻿using Module2_Task4.Models;
+using System.Collections;
+
+namespace Module2_Task4.Helpers
+{
+    class AnimalsComparer : IComparer
+    {
+        public int Compare(object i1, object i2)
+        {
+            var a = i1 as ParkInhabitant;
+            var b = i2 as ParkInhabitant;
+
+            if (a.DateOfBirth > b.DateOfBirth)
+            {
+                return -1;
+            }
+            else if (a.DateOfBirth < b.DateOfBirth)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
